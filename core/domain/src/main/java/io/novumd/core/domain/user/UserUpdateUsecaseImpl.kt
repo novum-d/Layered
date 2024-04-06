64ba4fee1898e5efe537e6b142cb34a2f4ecbda7
novumd <hamada.tomoki@email.com>
2024-04-06 14:52:55 +0900

    🏗️ chore: update source

--- core/domain/src/main/java/io/novumd/core/domain/user/UserUpdateUsecaseImpl.kt
+++ core/domain/src/main/java/io/novumd/core/domain/user/UserUpdateUsecaseImpl.kt
@@ -3,7 +3,6 @@ package io.novumd.core.domain.user
 import arrow.core.raise.Raise
 import arrow.core.raise.ensure
 import io.novumd.core.data.UserRepository
-import io.novumd.core.domain.UserExistsDomainService
 import io.novumd.core.domain.UserUpdateUsecase
 import io.novumd.core.model.Err
 import io.novumd.core.model.UserUpdateUsecaseError
@@ -13,14 +12,11 @@ import io.novumd.core.model.user.asExternalModel
 
 internal class UserUpdateUsecaseImpl(
   private val userRepository: UserRepository,
-  private val userExistsDomainService: UserExistsDomainService,
 ) : UserUpdateUsecase {
 
   context (Raise<UserUpdateUsecaseError>)
   override fun invoke(command: UserUpdateCommand) {
     val userId = command.id.let(::UserId)
-    userExistsDomainService(userId)
-
     val user = userRepository.find(userId) ?: raise(Err.UserNotFound)
 
     ensure(command.password == user.password.value) {
