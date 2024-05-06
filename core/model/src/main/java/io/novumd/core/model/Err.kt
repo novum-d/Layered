7dc0a9a43e3063274c3de9b2d1184250e2b285c2
novumd <hamada.tomoki@email.com>
2024-05-06 14:35:52 +0900

    🏗️ chore: update source

--- core/model/src/main/java/io/novumd/core/model/Err.kt
+++ core/model/src/main/java/io/novumd/core/model/Err.kt
@@ -4,24 +4,24 @@ import arrow.core.NonEmptyList
 
 
 fun example() {
-  val err: UserRegisterUseCaseError = Err.Data.NetworkError
-  when (err) {
-    // Domain Error
-    Err.Domain.UserIdAlreadyExists -> TODO()
-    Err.Domain.UserEmailAlreadyExists -> TODO()
-    is UserInvalidError -> err.nel.forEach {
-      when (it) {
-        Err.Domain.UserInvalid.Email -> TODO()
-        Err.Domain.UserInvalid.Name -> TODO()
-        Err.Domain.UserInvalid.Password -> TODO()
-      }
-    }
+    when (val err: UserRegisterUseCaseError = Err.Data.NetworkError) {
+        // Domain Error
+        Err.Domain.UserIdAlreadyExists -> TODO()
+        Err.Domain.UserEmailAlreadyExists -> TODO()
+        is UserInvalidError -> err.nel.forEach {
+            when (it) {
+                Err.Domain.UserInvalid.Id -> TODO()
+                Err.Domain.UserInvalid.Email -> TODO()
+                Err.Domain.UserInvalid.Name -> TODO()
+                Err.Domain.UserInvalid.Password -> TODO()
+            }
+        }
 
-    // Data Error
-    Err.Data.NetworkError -> TODO()
-    Err.Data.UnexpectedError -> TODO()
-    Err.Data.DatabaseError -> TODO()
-  }
+        // Data Error
+        Err.Data.NetworkError -> TODO()
+        Err.Data.UnexpectedError -> TODO()
+        Err.Data.DatabaseError -> TODO()
+    }
 }
 
 /** Domain Layer error type */
@@ -38,7 +38,7 @@ sealed interface UserEmailExistsDomainServiceError : UserRegisterUseCaseError, U
 
 /* ValidationError */
 data class UserInvalidError(
-  val nel: NonEmptyList<Err.Domain.UserInvalid>,
+    val nel: NonEmptyList<Err.Domain.UserInvalid>,
 ) : UserRegisterUseCaseError, UserUpdateUseCaseError
 
 /** Data Layer error type */
@@ -47,7 +47,7 @@ sealed interface UserRegisterDataError : UserRegisterUseCaseError
 sealed interface UserCreateIdDataError : UserRegisterUseCaseError
 sealed interface UserSaveDataError : UserUpdateUseCaseError
 sealed interface UserFindDataError : UserUpdateUseCaseError,
-  UserIdExistsDomainServiceError, UserEmailExistsDomainServiceError
+    UserIdExistsDomainServiceError, UserEmailExistsDomainServiceError
 
 sealed interface AppTamperedWithDataError : UserUpdateUseCaseError
 
@@ -55,30 +55,31 @@ sealed interface AppTamperedWithDataError : UserUpdateUseCaseError
 /** Handled Error Type */
 sealed interface Err {
 
-  sealed interface Domain : Err {
-    sealed interface UserInvalid : Domain, UserUpdateUseCaseError {
-      data object Name : UserInvalid
-      data object Password : UserInvalid
-      data object Email : UserInvalid
+    sealed interface Domain : Err {
+        sealed interface UserInvalid : Domain, UserUpdateUseCaseError {
+            data object Id : UserInvalid
+            data object Name : UserInvalid
+            data object Password : UserInvalid
+            data object Email : UserInvalid
+        }
+
+        data object PasswordNotMatched : Domain, UserUpdateUseCaseError
+        data object UserNotFound : Domain, UserUpdateUseCaseError
+        data object UserIdAlreadyExists : Domain, UserIdExistsDomainServiceError
+        data object UserEmailAlreadyExists : Domain, UserEmailExistsDomainServiceError
     }
 
-    data object PasswordNotMatched : Domain, UserUpdateUseCaseError
-    data object UserNotFound : Domain, UserUpdateUseCaseError
-    data object UserIdAlreadyExists : Domain, UserIdExistsDomainServiceError
-    data object UserEmailAlreadyExists : Domain, UserEmailExistsDomainServiceError
-  }
+    sealed interface Data : Err {
+        data object UnexpectedError : Data,
+            UserRegisterDataError, UserSaveDataError, UserCreateIdDataError, UserFindDataError,
+            AppTamperedWithDataError
 
-  sealed interface Data : Err {
-    data object UnexpectedError : Data,
-      UserRegisterDataError, UserSaveDataError, UserCreateIdDataError, UserFindDataError,
-      AppTamperedWithDataError
+        data object DatabaseError : Data,
+            UserRegisterDataError, UserSaveDataError, UserFindDataError,
+            AppTamperedWithDataError
 
-    data object DatabaseError : Data,
-      UserRegisterDataError, UserSaveDataError, UserFindDataError,
-      AppTamperedWithDataError
-
-    data object NetworkError : Data,
-      UserRegisterDataError, UserSaveDataError, UserCreateIdDataError, UserFindDataError,
-      AppTamperedWithDataError
-  }
+        data object NetworkError : Data,
+            UserRegisterDataError, UserSaveDataError, UserCreateIdDataError, UserFindDataError,
+            AppTamperedWithDataError
+    }
 }
--- core/model/src/main/java/io/novumd/core/model/user/UserValue.kt
+++ core/model/src/main/java/io/novumd/core/model/user/UserValue.kt
@@ -5,38 +5,46 @@ import arrow.core.raise.ensure
 import io.novumd.core.model.Err
 
 @JvmInline
-value class UserId(val value: String)
+value class UserId(val value: String) {
+    context(Raise<Err.Domain.UserInvalid.Id>)
+    fun validate() {
+        val regex = """^[a-zA-Z0-9_]+$""".toRegex()
+        ensure(regex.matches(value)) {
+            Err.Domain.UserInvalid.Id
+        }
+    }
+}
 
 @JvmInline
 value class UserName(val value: String) {
-  context(Raise<Err.Domain.UserInvalid.Name>)
-  fun validate() {
-    val maxLength = 3
-    ensure(value.length > maxLength) {
-      Err.Domain.UserInvalid.Name
+    context(Raise<Err.Domain.UserInvalid.Name>)
+    fun validate() {
+        val maxLength = 3
+        ensure(value.length > maxLength) {
+            Err.Domain.UserInvalid.Name
+        }
     }
-  }
 }
 
 @JvmInline
 value class UserEmail(val value: String) {
-  context(Raise<Err.Domain.UserInvalid.Email>)
-  fun validate() {
-    val pattern = """^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$"""
-    ensure(pattern.toRegex().matches(value)) {
-      Err.Domain.UserInvalid.Email
+    context(Raise<Err.Domain.UserInvalid.Email>)
+    fun validate() {
+        val regex = """^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$""".toRegex()
+        ensure(regex.matches(value)) {
+            Err.Domain.UserInvalid.Email
+        }
     }
-  }
 }
 
 @JvmInline
 value class UserPassword(val value: String) {
-  context(Raise<Err.Domain.UserInvalid.Password>)
-  fun validate() {
-    val minLength = 22
-    val pattern = """\d{4}"""
-    ensure(value.length > minLength && """^$pattern$""".toRegex().matches(value)) {
-      Err.Domain.UserInvalid.Password
+    context(Raise<Err.Domain.UserInvalid.Password>)
+    fun validate() {
+        val minLength = 22
+        val regex = """^\d{4}$""".toRegex()
+        ensure(value.length > minLength && regex.matches(value)) {
+            Err.Domain.UserInvalid.Password
+        }
     }
-  }
 }
